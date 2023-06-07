5004cb391feb21e3e0423907e1b19adc9cc54560
Даниил Дрогобужский <DanD23@yandex.ru>
2023-06-07 21:40:20 +0300

    new

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SKEY'
+SECRET_KEY = os.getenv('SKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
